8d926b22c835d3ce371205bcfb3ca933e0ee7fd4
Nikhil Sharma <nikhil@Nikhils-MacBook-Air.local>
2025-02-28 13:55:23 +0530

    changes

--- middleware.ts
+++ middleware.ts
@@ -15,4 +15,16 @@ export default auth(async function middleware(request) {
 
 export const config = {
 	matcher: ['/((?!api/auth|auth|images|_next/static|auth/*|_next/image|favicon.ico|api/cloudinary/image-upload|^/$).+)'],
+	unstable_allowDynamic: [
+		'/node_modules/@nextui-org/calendar/dist/chunk-NABLCSM5.mjs',
+		'/node_modules/@nextui-org/theme/dist/chunk-YSA7EQBH.mjs',
+		'/node_modules/@nextui-org/calendar/dist/index.mjs',
+		'/node_modules/@nextui-org/react/dist/index.mjs',
+		'/node_modules/@nextui-org/theme/dist/index.mjs',
+		'/node_modules/lodash.kebabcase/index.js',
+		'/node_modules/lodash.debounce/index.js',
+		'/node_modules/lodash.mapkeys/index.js',
+		'/node_modules/lodash.omit/index.js',
+		'/node_modules/lodash.get/index.js',
+	],
 }
